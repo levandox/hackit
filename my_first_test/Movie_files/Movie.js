af22a7c59da4920d017a6d5faa8117c966b76a98
vando <1033005000@qq.com>
2015-03-21 11:24:01 +0800

    inital commit

---
+++ my_first_test/Movie_files/Movie.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.2 local-build-20150320
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Movie_files/MovieMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
